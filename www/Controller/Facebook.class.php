c4a1b46aabd7279905f0ba883d5fd743880a92ba
Amine <aminecheriguifr@gmail.com>
2022-07-12 09:16:38 +0200

    first commit

--- www/Controller/Facebook.class.php
+++ www/Controller/Facebook.class.php
@@ -1,13 +1,22 @@
 <?php
 
-
-
 namespace App\Controller;
+
+use App\Core\Session;
+use App\Provider\FacebookProvider;
 
 class Facebook extends BaseController
 {
-    public function facebookOauthSuccess()
+    public function loginWithFacebook()
     {
-        echo "twitch_oauth_success";
+        $discordProvider = new DiscordProvider(
+            DISCORD_CLIENT_ID,
+            DISCORD_CLIENT_SECRET,
+            DISCORD_REDIRECT_URI,
+            DISCORD_SCOPE,
+        );
+
+        header("Location: " . $discordProvider->getAuthorizationUrl());
+
     }
 }
--- www/View/home.view.php
+++ www/View/home.view.php
@@ -4,6 +4,7 @@
 
 
 <a href="/loginWithTwitch">Se connecter avec Twitch</a>
-
 <br>
 <a href="/loginWithDiscord">Se connecter avec Discord</a>
+<br>
+<a href="/loginWithFacebook">Se connecter avec Facebook</a>
